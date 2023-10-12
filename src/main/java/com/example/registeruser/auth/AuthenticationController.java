e9f1bc3944cda1071ac2f733a8b5076c2558b8ae
Gino <ginowine1@gmail.com>
2023-10-12 12:16:59 +0100

    springboot authorization and user registration

---
+++ src/main/java/com/example/registeruser/auth/AuthenticationController.java
@@ -0,0 +1,28 @@
+package com.example.registeruser.auth;
+
+import com.example.registeruser.user.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    private final AuthenticationService service;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register (@RequestBody RegisterRequest request){
+        // implement later
+        return ResponseEntity.ok(service.register(request));
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate (@RequestBody AuthenticationRequest request){
+        // implement later
+        return ResponseEntity.ok(service.authenticate(request));
+    }
+}
---
+++ src/main/java/com/example/registeruser/user/Role.java
@@ -0,0 +1,7 @@
+package com.example.registeruser.user;
+
+public enum Role {
+
+    USER,
+    ADMIN
+}
